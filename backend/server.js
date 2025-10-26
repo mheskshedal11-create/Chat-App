0ed38609767069fbdbdb04282e318fb2b8e2078c
mahesh kumar chhedal <mheskshedal11@gmail.com>
2025-10-26 13:02:21 +0545

    create message schema file

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,18 @@ import express from 'express'
 import "dotenv/config"
 import cors from 'cors'
 import { connectDB } from './lib/db.js'
+import userRouter from './routes/user.router.js'
 
 
 const app = express()
+
 await connectDB()
-app.use(express.json({ limit: '4mb' }))
-app.use(cors())
 const PORT = process.env.PORT || 8000
+app.use(express.json({ limit: '4mb' }))
+
+app.use(cors())
+
+app.use('api/v1/user', userRouter)
 
 app.listen(PORT, () => {
     console.log(`server is running http://localhost:${PORT}`)
